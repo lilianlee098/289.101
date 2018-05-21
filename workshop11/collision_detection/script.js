857769e96ac80c7bb92e2833ed1b93368c139d3b
Lee <16400777@wsom-072801.massey.ac.nz>
2018-05-21 14:09:46 +1200

    added workshop11 and aded in tutorial files and starting new files and folders for if statements

---
+++ workshop11/collision_detection/script.js
@@ -0,0 +1,11 @@
+/*
+
+Begin by following the steps in the workshop notes.
+
+Next, add the following features:
+- the gap goes red when the puck is over it
+- the puck 'teleports' to the opposite boundary when it crosses one (think asteroids/snake)
+- create an item that makes the puck bigger
+- create an item that makes the puck smaller
+
+*/
